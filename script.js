1facb86fc09a4c81b94fa3596905a9e8ccdd4dd1
kokofour <117838925+kokofour@users.noreply.github.com>
2025-04-07 18:49:07 -0400

    Feature: Added document.querySelector()
    This shows how document.querySelector() is used in script.js to select elements in index.html
    - Added code to select the elements by ID, class and tag name
    - Logged the selected elements to the console to verify the selection
    
    document.querySelector allows selecting the first element that matches a CSS selector

--- script.js
+++ script.js
@@ -10,7 +10,13 @@ let titleElement = document.getElementById("title");
 console.log(titleElement)
 
 
+let heading = document.querySelector("#main-heading"); // Select by ID
+let paragraph = document.querySelector(".description"); // Select by class
+let button = document.querySelector("button"); // Select by tag name
 
+console.log(heading);
+console.log(paragraph);
+console.log(button);
 
 
 
