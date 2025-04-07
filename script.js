79dd7a377b13a3d1156441080b360cdcca5b079f
kokofour <117838925+kokofour@users.noreply.github.com>
2025-04-07 17:01:59 -0400

    Based on my understanding, the getElementById enables the developer to directly access and manipulate the HTML element via javascript. getElementByID returns a single element object.
    How it works:
     You call the method on the document object.
     It passes the id of the element you want to select as a string to the method
     Then, it searches for the HTML document for the element with that specific id.

---
+++ script.js
@@ -0,0 +1,49 @@
+//document.addEventListener("DOMContentLoaded", function()) {
+   // const title = document.getElementById("title");
+    //console.log(titleElement);
+//}
+//const pTag = document.querySelector
+//const pTag2 = document.querySelector
+
+
+let titleElement = document.getElementById("title");
+console.log(titleElement)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
